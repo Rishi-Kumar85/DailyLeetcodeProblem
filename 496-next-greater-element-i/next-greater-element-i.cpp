887bcc787ff7e582f238ab2a317f6947b02f118f
Rishi Kumar <rishikumar852125@gmail.com>
2026-08-25 16:14:56 +0530

    Time: 0 ms (100.00%) | Memory: 12.8 MB (62.72%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -1,34 +1,33 @@
 class Solution {
 public:
-       vector<int> nextGreaterElementArray(vector<int>& nums) {
-        int n = nums.size();
-
-        vector<int> ans(n, -1);
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
         stack<int> st;
+        unordered_map<int, int> mp;
 
-        for (int i = 0; i < n; i++) {
+        // Find next greater element for every element in nums2
+        for (int i = 0; i < nums2.size(); i++) {
 
-            while (!st.empty() && nums[st.top()] < nums[i]) {
-                ans[st.top()] = nums[i];
+            while (!st.empty() && st.top() < nums2[i]) {
+                mp[st.top()] = nums2[i];
                 st.pop();
             }
 
-            st.push(i);
+            st.push(nums2[i]);
         }
 
-        return ans;
-    }
-    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        vector<int>ng=nextGreaterElementArray(nums2);
-        vector<int>ans(nums1.size(),-1);
-        for(int i=0;i<nums1.size();i++){
-            for(int j=0;j<nums2.size();j++){
-                if(nums1[i]==nums2[j]){
-                    ans[i]=ng[j];
-                    break;
-                }
-            }
+        // Elements remaining in stack have no greater element
+        while (!st.empty()) {
+            mp[st.top()] = -1;
+            st.pop();
         }
+
+        // Build answer for nums1
+        vector<int> ans;
+
+        for (int x : nums1) {
+            ans.push_back(mp[x]);
+        }
+
         return ans;
     }
 };
