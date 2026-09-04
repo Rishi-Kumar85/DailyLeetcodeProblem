dba7aca2293e0dcb768787d46b3668e02ff553dd
Rishi Kumar <rishikumar852125@gmail.com>
2026-09-04 23:10:45 +0530

    Time: 2 ms (29.25%) | Memory: 30.2 MB (94.42%) - LeetSync

---
+++ 4284-smallest-stable-index-i/smallest-stable-index-i.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int firstStableIndex(vector<int>& nums, int k) {
+        int n = nums.size();
+        for(int i=0;i<n;i++){
+           int maximum=*max_element(nums.begin(),nums.begin()+i+1);
+           int min = *min_element(nums.begin()+i,nums.end());
+           if(maximum-min<=k) return i;
+        }
+        return -1;
+    }
+};
