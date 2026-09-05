68f2bde7aca503f06743aca3b667853861e1da27
Rishi Kumar <rishikumar852125@gmail.com>
2026-09-05 23:52:26 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (59.49%) - LeetSync

--- 650-2-keys-keyboard/2-keys-keyboard.cpp
+++ 650-2-keys-keyboard/2-keys-keyboard.cpp
@@ -1,28 +1,13 @@
 class Solution {
 public:
-    bool isPrime(int n){
-        for(int i=2;i*i<=n;i++){
-            if(n%i==0) return false;
-        }
-        return true;
-    }
-    int gd(int n){
-        for(int i=2;i*i<=n;i++){
-            if(n%i==0) return n/i;
-        }
-        return 1;
-    }
     int minSteps(int n) {
-        int count=0;
-        while(n>1){
-            if(isPrime(n)){
-                count+=n;
-                break;
-            }
-            int hf=gd(n);
-            count+=n/hf;
-            n=hf;
+       int ans = 0;
+    for (int i = 2; i <= n; i++) {
+        while (n % i == 0) {
+            ans += i;
+            n /= i;
         }
-        return count;
+    }
+    return ans;
     }
 };
