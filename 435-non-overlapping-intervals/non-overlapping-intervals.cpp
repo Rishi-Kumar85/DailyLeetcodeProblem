1e5667dbda8ab174374603ef754e10c01f456621
Rishi Kumar <rishikumar852125@gmail.com>
2026-09-07 00:09:37 +0530

    Time: 44 ms (60.69%) | Memory: 94.2 MB (21.25%) - LeetSync

--- 435-non-overlapping-intervals/non-overlapping-intervals.cpp
+++ 435-non-overlapping-intervals/non-overlapping-intervals.cpp
@@ -8,29 +8,16 @@ public:
                  return a[1] < b[1];
              });
 
-        int removed = 0;
+       int prev = 0;
+        int count = 1;
 
-        int previousEnd = intervals[0][1];
-
-        for (int i = 1; i < intervals.size(); i++) {
-
-            // Overlap
-            if (intervals[i][0] < previousEnd) {
-
-                removed++;
-
-                // Keep the interval that ends earlier
-                previousEnd =
-                    min(previousEnd, intervals[i][1]);
-            }
-            else {
-
-                // No overlap
-                previousEnd = intervals[i][1];
+        for(int i = 1; i < intervals.size(); i++){
+            if(intervals[i][0] >= intervals[prev][1]){
+                prev = i;
+                count++;
             }
         }
-
-        return removed;
+        return intervals.size() - count;
 
     }
 };
