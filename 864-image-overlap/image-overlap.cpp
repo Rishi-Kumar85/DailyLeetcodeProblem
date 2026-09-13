a80459b9b23fa77c86df820f2d1446c808645103
Rishi Kumar <rishikumar852125@gmail.com>
2026-09-14 00:44:51 +0530

    Time: 149 ms (12.78%) | Memory: 16.1 MB (21.97%) - LeetSync

--- 864-image-overlap/image-overlap.cpp
+++ 864-image-overlap/image-overlap.cpp
@@ -1,31 +1,45 @@
 class Solution {
 public:
-    int largestOverlap(vector<vector<int>>& img1, vector<vector<int>>& img2) {
-        vector<pair<int,int>>ones1;
-        vector<pair<int,int>>ones2;
+    int largestOverlap(vector<vector<int>>& img1,
+                       vector<vector<int>>& img2) {
 
-        for(int r=0;r<img1.size();r++){
-            for(int c=0;c<img1[0].size();c++){
-                if(img1[r][c]==1) ones1.push_back({r,c});
-                if(img2[r][c]==1) ones2.push_back({r,c});
+        int n = img1.size();
 
+        vector<pair<int, int>> ones1;
+        vector<pair<int, int>> ones2;
+
+        // Store positions of 1s in img1
+        for (int r = 0; r < n; r++) {
+            for (int c = 0; c < n; c++) {
+
+                if (img1[r][c] == 1) {
+                    ones1.push_back({r, c});
+                }
+
+                if (img2[r][c] == 1) {
+                    ones2.push_back({r, c});
+                }
             }
         }
 
-        map<pair<int,int>,int>mp;
-          int ans=0;
-        for(auto p1 : ones1){
-              for(auto p2: ones2){
-                int dr = p2.first-p1.first;
-                int dc = p2.second-p1.second;
-                mp[{dr,dc}]++;
+        map<pair<int, int>, int> count;
 
-                ans = max(ans,mp[{dr,dc}]);
-              }
+        int ans = 0;
+
+        // Compare every 1 in img1 with every 1 in img2
+        for (auto p1 : ones1) {
+
+            for (auto p2 : ones2) {
+
+                int dr = p2.first - p1.first;
+                int dc = p2.second - p1.second;
+
+                count[{dr, dc}]++;
+
+                ans = max(ans, count[{dr, dc}]);
+            }
         }
-             
-             return ans;
 
-
+        return ans;
     }
 };
