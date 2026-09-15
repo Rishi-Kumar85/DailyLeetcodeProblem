180f49ff3e33a72e2062ba7d61499739cc6b4901
Rishi Kumar <rishikumar852125@gmail.com>
2026-09-16 00:39:05 +0530

    Time: 7 ms (76.70%) | Memory: 8.8 MB (95.55%) - LeetSync

--- 2559-maximum-number-of-non-overlapping-palindrome-substrings/maximum-number-of-non-overlapping-palindrome-substrings.cpp
+++ 2559-maximum-number-of-non-overlapping-palindrome-substrings/maximum-number-of-non-overlapping-palindrome-substrings.cpp
@@ -1,51 +1,22 @@
 class Solution {
 public:
     int maxPalindromes(string s, int k) {
-
-        int n = s.size();
-
-        // palindrome[i][j] = true if s[i...j] is palindrome
-        vector<vector<bool>> palindrome(
-            n, vector<bool>(n, false)
-        );
-
-        // Build palindrome table
-        for (int i = n - 1; i >= 0; i--) {
-
-            for (int j = i; j < n; j++) {
-
-                if (s[i] == s[j] &&
-                    (j - i <= 1 || palindrome[i + 1][j - 1])) {
-
-                    palindrome[i][j] = true;
+        int n = s.size(), ans = 0, end = -1;
+        for(int i = 0; i < n; i++){
+            // Even Length first, then Odd Length
+            for(int l : {i - 1, i}){
+                int r = i;
+                // expand outward from the center
+                while(l >= 0 && r < n && s[l] == s[r]){
+                    if(r - l + 1 >= k && l > end){
+                        ans++;
+                        end = r;
+                        break;
+                    }
+                    l--, r++;
                 }
             }
         }
-
-        // dp[i] = maximum number of palindromes
-        // using first i characters
-        vector<int> dp(n + 1, 0);
-
-        for (int i = 0; i < n; i++) {
-
-            // Don't take a palindrome ending here
-            dp[i + 1] = dp[i];
-
-            for (int j = 0; j <= i; j++) {
-
-                int length = i - j + 1;
-
-                if (length >= k &&
-                    palindrome[j][i]) {
-
-                    dp[i + 1] = max(
-                        dp[i + 1],
-                        dp[j] + 1
-                    );
-                }
-            }
-        }
-
-        return dp[n];
+        return ans;
     }
 };
