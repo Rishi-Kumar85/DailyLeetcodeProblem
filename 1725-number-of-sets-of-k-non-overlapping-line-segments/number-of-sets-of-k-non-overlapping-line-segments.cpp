58b3782d7b0e61b181429f87a6878d4c2ab36f67
Rishi Kumar <rishikumar852125@gmail.com>
2026-09-17 01:06:13 +0530

    Time: 39 ms (59.24%) | Memory: 53 MB (33.18%) - LeetSync

--- 1725-number-of-sets-of-k-non-overlapping-line-segments/number-of-sets-of-k-non-overlapping-line-segments.cpp
+++ 1725-number-of-sets-of-k-non-overlapping-line-segments/number-of-sets-of-k-non-overlapping-line-segments.cpp
@@ -1,42 +1,22 @@
 class Solution {
 public:
+    static const int MOD = 1e9 + 7;
+
     int numberOfSets(int n, int k) {
+        vector<vector<long long>> dp(n, vector<long long>(k + 1));
+        // 0 segments → exactly 1 way
+        for(int i = 0; i < n; i++) dp[i][0] = 1;
 
-        const int MOD = 1000000007;
-
-        // dp[i] = ways for the previous number of segments
-        vector<int> dp(n + 1, 1);
-
-        dp[0] = 0;
-
-        // prefix sum of dp
-        vector<int> prefix(n + 1, 0);
-
-        for (int i = 1; i <= n; i++) {
-            prefix[i] = (prefix[i - 1] + dp[i]) % MOD;
-        }
-
-        for (int j = 1; j <= k; j++) {
-
-            vector<int> new_dp(n + 1, 0);
-            vector<int> new_prefix(n + 1, 0);
-
-            for (int i = 2; i <= n; i++) {
-
-                // Don't use this point as the new endpoint
-                // OR use it to create/extend a segment
-                new_dp[i] =
-                    (new_dp[i - 1] + prefix[i - 1]) % MOD;
-
-                // Build prefix sum for the new row
-                new_prefix[i] =
-                    (new_prefix[i - 1] + new_dp[i]) % MOD;
+        for(int j = 1; j <= k; j++){
+            long long sum = 0;
+            for(int i = 1; i < n; i++){
+                // Add ways for j-1 segments
+                sum = (sum + dp[i - 1][j - 1]) % MOD;
+                // Don't use i OR end a segment at i
+                dp[i][j] = (dp[i - 1][j] + sum) % MOD;
             }
-
-            dp = new_dp;
-            prefix = new_prefix;
         }
 
-        return dp[n];
+        return dp[n - 1][k];
     }
 };
